02c0953b99cc157fee80feada53cd772fdba9dd6
Jhonattan Loli Mazzuco <jhonattan.mazzuco.ext@esferaenergia.com.br>
2024-12-11 19:49:07 -0300

    change reply konservaroco

--- src/commands/kakaroco/kakaroco.commands.ts
+++ src/commands/kakaroco/kakaroco.commands.ts
@@ -52,7 +52,11 @@ export class KakarocoCommands {
           'https://cdn.discordapp.com/attachments/1152459154846384180/1290654835812601978/OIG3.J.jpg?ex=6759887c&is=675836fc&hm=a3685fb0853d5419d30990920a183470733c2873dce8c108a87ac57a3e77552a&',
         )
         .setDescription(this._getRandomMessage());
-      return interaction.reply({ embeds: [embed], ephemeral: true });
+      return interaction
+        .deferReply()
+        .then(() =>
+          interaction.editReply({ embeds: [embed] }),
+        );
     } catch (e) {
       this.logger.error(e.message);
       return interaction.reply({
